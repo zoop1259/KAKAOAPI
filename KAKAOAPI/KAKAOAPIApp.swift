eafba130f4a4160603fb459640d9b303df25d2a3
zoop1259 <kdm43211991@gmail.com>
2023-02-04 18:12:43 +0900

    Initial Commit

---
+++ KAKAOAPI/KAKAOAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KAKAOAPIApp.swift
+//  KAKAOAPI
+//
+//  Created by 강대민 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct KAKAOAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
