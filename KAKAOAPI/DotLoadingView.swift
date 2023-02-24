8c678d313aa01241cd7ab011f5d54217d4a7c85f
zoop1259 <kdm43211991@gmail.com>
2023-02-24 18:52:08 +0900

    loadingView, 임시툴팁view

---
+++ KAKAOAPI/DotLoadingView.swift
@@ -0,0 +1,62 @@
+//
+//  DotLoadingView.swift
+//  KAKAOAPI
+//
+//  Created by 강대민 on 2023/02/24.
+//
+
+import SwiftUI
+//챗 응답 대기용..
+struct DotLoadingView: View {
+    
+    @State private var showCircle1 = false
+    @State private var showCircle2 = false
+    @State private var showCircle3 = false
+    
+    var body: some View {
+        HStack {
+            Circle()
+                .opacity(showCircle1 ? 1 : 0)
+            Circle()
+                .opacity(showCircle2 ? 1 : 0)
+            Circle()
+                .opacity(showCircle3 ? 1 : 0)
+        }
+        .foregroundColor(.gray.opacity(0.5))
+        .onAppear { performAnimation() }
+    }
+    
+    //0.4초마다 하나씩...
+    func performAnimation() {
+        let animation = Animation.easeInOut(duration: 0.4)
+        withAnimation(animation) {
+            self.showCircle1 = true
+            self.showCircle3 = false
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            withAnimation(animation) {
+                self.showCircle2 = true
+                self.showCircle1 = false
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            withAnimation(animation) {
+                self.showCircle2 = false
+                self.showCircle3 = true
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+            self.performAnimation()
+        }
+    }
+}
+
+struct DotLoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        DotLoadingView()
+    }
+}
+
